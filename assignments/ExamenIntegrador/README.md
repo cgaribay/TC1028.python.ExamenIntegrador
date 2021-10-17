32087495a90b1cf0ef1960978bfaa1f9f93f5d53
Carlos Garibay <carlos.garibay@tec.mx>
2021-10-17 15:40:27 -0500

    Initial commit

--- assignments/ExamenIntegrador/README.md
+++ assignments/ExamenIntegrador/README.md
@@ -1,28 +1,13 @@
 ![Tec de Monterrey](../../images/logotecmty.png)
-# cuenta pares e impares
-## Tema: Listas
+# Examen Integrador
 
-Modifica el programa que se encuentra en la carpeta `src` que se llama `exercise.py` y que contiene el siguiente código:
-
-```python
-def main():
-  #escribe tu código abajo de esta línea
-
-if __name__ == '__main__':
-    main()
-```
-
-La línea `#escribe tu código abajo de esta línea` es un comentario, el programa la va a ignorar al ejecutarse.
-
-Diseña e implementa un programa que determine la cuenta de valores pares e impares de los elementos de una lista de números enteros que recibirá en su entrada. Los valores los introduce el usuario uno por uno, se van almacenando en una lista y posteriormente se analizará la lista para determinar cuantos valores pares e impares posee. Consideramos al 0 como par.
+Realiza un programa en Python que cargue la información de los archivos productos.csv, vendedores.csv y ventas.csv a las matriz de productos, vendedores y ventas (**Esto NO lo tienes que hacer, ya viene incluido en el código de inicio en github**). Posteriormente crea y manda llamar una función llamada **revisar_metas**. Esta función deberá preguntar el nombre de un vendedor y calcular el dinero generado hasta el por las ventas del vendedor y, si ese dinero es mayor o igual a la meta de ese vendedor (la meta se encuentra en el archivo de vendedores.csv y se guarda en la matriz de ventas), entonces se debe imprimir el mensaje "Felicidades por alcanzar la meta de ventas". Si no se ha alcanzado esa meta deberás imprimir un mensaje informando la cantidad de dinero que le falta al vendedor para llegar a la meta.
 
 ## Entrada
-Cero o más valores enteros, uno en cada renglón. Finaliza la captura con un *.
+El nombre del vendedor, en mayúsculas o minúsculas.
 
 ## Salida
-Se despliega el número de pares e impares que tiene la lista con el siguiente formato: 
-PARES=3 o IMPARES=2 cada uno en un renglón. 
-El desplegado de la salida consiste en la palabra PARES seguida de un = y luego un número que representa el número de pares en la lista; posteriormente la palabra IMPARES, seguida de un = y luego el número que representa el número de impares que hay en la lista. Cada mensaje en un renglón y en ese orden.
+El mensaje "Felicidades por alcanzar la meta de ventas", si el vendedor alcanzó la meta o el mensaje "Te falta X para alcanzar la meta" si aún no se alcanza la meta.
 
 ## Ejemplo de ejecución del programa
 ### Entrada
--- assignments/ExamenIntegrador/src/exercise.py
+++ assignments/ExamenIntegrador/src/exercise.py
@@ -1,36 +1,93 @@
+
+from pathlib import Path
+import productos
+import vendedores
+import ventas
+
+lista_productos = []
+
+lista_vendedores = []
+
+lista_ventas = []
+
+# print_matriz
+# matriz: la matriz que quieres desplegar en la terminal
+# nombre_columnas: una lista con los nombres de cada columna de la matriz
+def print_matriz(matriz, nombre_columnas):
+    bigger_size = []
+    for nombre in nombre_columnas:
+        bigger_size.append(len(nombre))
+    
+    for idx, fila in enumerate(matriz):
+        for element in fila:
+            if len(str(element)) > bigger_size[idx]:
+                bigger_size[idx] = len(str(element))
+
+    for i in range(len(nombre_columnas)):
+        print(nombre_columnas[i].center(bigger_size[i] + 2), end = "")
+    print()
+    
+    for i in range(len(matriz[0])):
+        for j in range(len(nombre_columnas)):
+            print(str(matriz[j][i]).title().center(bigger_size[j] + 2), end = "")
+        print()
+
+# buscar_elemento
+# matriz: la tabla en la que deseas buscar
+# columna: la columna de la tabla en la que deseas buscar
+# valor: el valor que deseas buscar
+# return: el indice del valor en la tabla o -1 si no esta
+#              lista_vendedores, 1, mariana lopez
+def buscar_elemento(matriz, columna, valor):
+    if valor in matriz[columna]:
+        return matriz[columna].index(valor)
+    else:
+        return -1
+
+# obtener_id
+# matriz: la tabla en la que deseas encontrar el identificador
+# indice: el indice del elemento del que deseas encontrar el identificador o -1 si el indice es incorrecto
+def obtener_elemento(matriz, indice):
+    if indice < len(matriz[0]) and indice >= -len(matriz[0]):
+        return matriz[0][indice]
+    else:
+        return -1
+
+# Abrir el archivo de productos, leer su informacion y carga la informacion en lista_productos
+def cargar_productos():
+    ruta_productos = Path('archivos', 'productos.csv')
+    archivo_productos = open(ruta_productos)
+    contenido_productos = archivo_productos.readlines()
+    for line in contenido_productos:
+        lista_productos.append(line.strip().split(','))
+    archivo_productos.close()
+
+# Abrir el archivo de vendedores, leer su informacion y carga la informacion en lista_vendedores
+def cargar_vendedores():
+    ruta_vendedores = Path('archivos', 'vendedores.csv')
+    archivo_vendedores = open(ruta_vendedores)
+    contenido_vendedores = archivo_vendedores.readlines()
+    for line in contenido_vendedores:
+        lista_vendedores.append(line.strip().split(','))
+    archivo_vendedores.close()
+
+# Abrir el archivo de ventas, leer su informacion y carga la informacion en lista_ventas
+def cargar_ventas():
+    ruta_ventas = Path('archivos', 'ventas.csv')
+    archivo_ventas = open(ruta_ventas)
+    contenido_ventas = archivo_ventas.readlines()
+    for line in contenido_ventas:
+        lista_ventas.append(line.strip().split(','))
+    archivo_ventas.close()
+
 def main():
-    #escribe tu código abajo de esta línea
-    
-    # CODIGO ###############
+    cargar_productos()
+    cargar_vendedores()
+    cargar_ventas()
 
-    # Contando pares e impares -----------------------------
-    # Autor: ricardo.quintero@tec.mx
-
-    fin=False
-    lista=[]
-
-    while not fin:
-        numero=input()
-
-        if numero == "*":
-            break
-        lista.append(int(numero))
-
-    cp=0
-    ci=0
-
-    for e in lista:
-        if e % 2 == 0:
-            cp=cp+1
-        else:
-            ci=ci+1
-
-    print("PARES="+str(cp))
-    print("IMPARES="+str(ci))
-
-    # FIN CODIGO ######################
-    
-    pass
+    print_matriz(lista_productos, productos.COLUMNAS)
+    print_matriz(lista_vendedores, vendedores.COLUMNAS)
+    print_matriz(lista_ventas, ventas.COLUMNAS)
 
 if __name__=='__main__':
     main()
